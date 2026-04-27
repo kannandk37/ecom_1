e31e5036f5a48ff2265750bc3f6eb0a4161a9db0
kannan <kannandk37@gmail.com>
2026-04-27 16:56:13 +0530

    cros erro fix

--- src/service/index.ts
+++ src/service/index.ts
@@ -6,7 +6,7 @@ const axiosinstance = axios.create({
 
     timeout: 9000,
     headers: {
-        "Access-Control-Allow-Origin": "http://localhost:8080",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Header":
             "Origin, Content-Type, X-Requested-With, Accept, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Access-Control-Allow-Origin",
